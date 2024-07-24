dfdc6a241c9d7e2ecaaeb4c2eee5fa955ecfc8bc
h0rzn <hrzn@protonmail.com>
2024-07-24 21:39:07 +0200

    added wip parse cmd

--- language/validate.go
+++ language/validate.go
@@ -7,79 +7,74 @@ import (
 	sitter "github.com/smacker/go-tree-sitter"
 )
 
-var Reset = "\033[0m"
-var Red = "\033[31m"
-var Yellow = "\033[33m"
-
-// var Red = "\x1b[6;30;42m"
-// var Red = "\x1b[6;30;41m"
-
 func ValidateDocument(document *Document) {
 	var errNodes []*sitter.Node
 	errNodes = findErrorNodes(document.tree.RootNode(), errNodes)
-	printWithErrors(document.fileContents, errNodes)
-
-	fmt.Println("---")
-	n := document.fileContents[24:37]
-	fmt.Printf("%q\n", string(n))
+	printWithErrors(document, errNodes)
 }
 
-func printWithErrors(contents []byte, errNodes []*sitter.Node) {
+func printWithErrors(document *Document, errNodes []*sitter.Node) {
+	contents := document.fileContents
 	var errNodeRanges []sitter.Range
 	for _, errNode := range errNodes {
-		fmt.Println("errRange", errNode.Range())
 		errNodeRanges = append(errNodeRanges, errNode.Range())
 	}
 
-	var builder strings.Builder
-	var lineOffset uint32
-	var errOffset uint32
-	var errLen uint32
-	var errMessage string
-	var isErrorLine bool
-	var showErrorLine bool
+	renderer := NewRenderer(document)
+	// var lineOffset uint32
+	// var errOffset uint32
+	// var errLen uint32
+	// var errMessage string
+	// var isErrorLine bool
+	// var showErrorLine bool
 	for byteOffset, contentsByte := range contents {
-		lineOffset += 1
-		if contentsByte == '\n' {
-			lineOffset = 0
-			if isErrorLine && lineOffset == 0 {
-				showErrorLine = true
-				isErrorLine = false
-			}
-		}
-		if contentsByte == '\t' {
-			tabWidth := uint32(8 - (int(lineOffset) % 8))
-			lineOffset += tabWidth
-		}
+		// lineOffset += 1
+		// if contentsByte == '\n' {
+		// 	lineOffset = 0
+		// 	if isErrorLine && lineOffset == 0 {
+		// 		showErrorLine = true
+		// 		isErrorLine = false
+		// 	}
+		// }
+		// if contentsByte == '\t' {
+		// 	tabWidth := uint32(8 - (int(lineOffset) % 8))
+		// 	lineOffset += tabWidth
+		// }
 
 		for nodeRangeIndex, nodeRange := range errNodeRanges {
 			if byteOffset == int(nodeRange.StartByte) {
-				// fmt.Println("++ e start", nodeRange.StartByte, nodeRange.EndByte)
-				builder.WriteString(Red)
-				isErrorLine = true
-				errOffset = lineOffset
-				errLen = nodeRange.EndByte - nodeRange.StartByte
-				errMessage = errNodes[nodeRangeIndex].String()
+
+				errMessage := errNodes[nodeRangeIndex].String()
+				renderer.SetMarker(nodeRange.StartByte, nodeRange.EndByte, errMessage)
+
+				// renderer.SetColor(Red)
+
+				// isErrorLine = true
+				// errOffset = lineOffset
+				// errLen = nodeRange.EndByte - nodeRange.StartByte
+				// errMessage = errNodes[nodeRangeIndex].String()
 				break
 			}
 
 			// fix linewrap due to '\n'
-			if byteOffset == int(nodeRange.EndByte)+1 && contents[byteOffset+1] == '\n' {
-				// fmt.Println("-- err end", string(contentsByte))
-				builder.WriteString(Reset)
-				errOffset = 0
-				break
-			}
+			// if byteOffset == int(nodeRange.EndByte)+1 && contents[byteOffset+1] == '\n' {
+			// 	renderer.ResetColor()
+			// 	// errOffset = 0
+			// 	break
+			// }
 		}
-		builder.WriteByte(contentsByte)
-		if showErrorLine {
-			errorLine := createErrorLine(errOffset, errLen, errMessage)
-			builder.WriteString(errorLine)
-			showErrorLine = false
-		}
+		// builder.WriteByte(contentsByte)
+		renderer.Push(contentsByte)
+		// if showErrorLine {
+		// 	// errorLine := createErrorLine(errOffset, errLen, errMessage)
+		// 	errorLine := errMessage
+		// 	renderer.PushString(errorLine)
+		// 	showErrorLine = false
+		//
+		// }
 	}
 
-	fmt.Println(builder.String())
+	fmt.Println(renderer.String())
 }
 
 func createErrorLine(offset uint32, len uint32, message string) string {
@@ -92,7 +87,7 @@ func createErrorLine(offset uint32, len uint32, message string) string {
 	}
 	builder.WriteByte('\n')
 
-	errMessage := fmt.Sprintf("%s*\tError: %s%s\n", Yellow, message, Reset)
+	errMessage := fmt.Sprintf("%s*\tError: %s%s\n", colorYellow, message, colorReset)
 	builder.WriteString(errMessage)
 
 	return builder.String()
@@ -102,7 +97,7 @@ func findErrorNodes(node *sitter.Node, errNodes []*sitter.Node) []*sitter.Node {
 	for i := 0; i < int(node.ChildCount()); i++ {
 		child := node.Child(i)
 		if child.IsError() {
-			fmt.Println("found err, is", child.IsError(), "has", child.HasError(), child.String(), child.StartByte(), child.EndByte())
+			// fmt.Println("found err, is", child.IsError(), "has", child.HasError(), child.String(), child.StartByte(), child.EndByte())
 			if len(errNodes) != 0 {
 				previousIndex := len(errNodes) - 1
 				if child.Parent() == errNodes[previousIndex] {
