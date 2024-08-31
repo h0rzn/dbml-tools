5676d7570d8368c2c2579cbc5c6ea6d7d5f24d0e
h0rzn <hrzn@protonmail.com>
2024-08-31 15:29:11 +0200

    parser: support for composite indexes

--- tree-sitter-dbml/grammar_table_indexes.js
+++ tree-sitter-dbml/grammar_table_indexes.js
@@ -13,10 +13,22 @@ module.exports = {
 
   index_definition: $ => choice(
     $.index_definition_single,
+    $.index_definition_composite
   ),
 
-
   index_definition_single: $ => $.identifier,
+
+  index_definition_composite: $ => seq(
+    '(',
+    $.identifier,
+    repeat(
+      seq(
+        ',',
+        $.identifier
+      ),
+    ),
+    ')'
+  ),
 
   indexes: $ => token('indexes')
 
